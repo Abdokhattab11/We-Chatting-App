ec79ae25fc49435e2bc1489f359906d5f475d2f3
AbdoKhattab <mr.spykhattab@gmail.com>
2024-08-22 00:41:58 +0300

    Fixed Redis Connectiom

--- services/redisService.js
+++ services/redisService.js
@@ -1,4 +1,8 @@
 const redis = require("redis");
-const client = redis.createClient();
+const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+});
 
 module.exports = client;
