e628d92d4060649fbec9f9a48d554ce6540707d9
AbdoKhattab <mr.spykhattab@gmail.com>
2024-08-26 00:30:42 +0300

    fixed cors origin problem

--- app.js
+++ app.js
@@ -13,12 +13,24 @@ const {protect} = require("./services/authService");
 app.use(cookieParser());
 app.use(express.json());
 app.use(require("./middlewares/tokenCheckInRedisMiddleware"));
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.options("*", cors()); // include before other routes
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
