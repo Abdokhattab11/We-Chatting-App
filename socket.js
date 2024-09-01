1fb888bc103f717dd648579661f438c55e2d2d75
AbdoKhattab <mr.spykhattab@gmail.com>
2024-09-01 19:39:26 +0300

    Added Creation of room between users

--- socket.js
+++ socket.js
@@ -3,6 +3,7 @@ const socketIo = require('socket.io');
 const userModel = require("./models/userModel");
 const roomModel = require("./models/chatModel");
 const redisClient = require('./services/redisService');
+const uuid = require('./utils/uuid')
 
 
 module.exports = (server) => {
@@ -19,36 +20,37 @@ module.exports = (server) => {
         console.log(`Socket ${socket.id} is connected to the server`);
 
         socket.on('connect_user', async (userId) => {
+            console.log(`User of id ${userId} Is connected to this socket`);
             // 1. Make this socket belong to this userId
             socket.userId = userId;
             await redisClient.set(userId, socket.id);
         });
         socket.on('create_room', async (creatorInfo) => {
             const {senderId, receiverId} = creatorInfo;
-            const roomId = senderId + receiverId;
+
+            const roomId = "1"
+
             const user1 = await userModel.findById(senderId);
             const user2 = await userModel.findById(receiverId);
 
+
             // Make User 1 Join Room
             socket.join(roomId);
-
             const receiverSocketId = await redisClient.get(receiverId);
 
             io.sockets.sockets.get(receiverSocketId).join(roomId);
-
 
             const roomDate = await roomModel.create({
                 roomExternalId: roomId,
                 user1: user1._id,
                 user2: user2._id,
             });
-
-            socket.emit('room_created', (roomData) => {
-
-            });
+            console.log(`Chat Room ID : ${roomId}`);
+            console.log(`Chat Room Created Between user ${senderId} & ${receiverId}`);
         });
         socket.on('message', (messageData) => {
-            consle.log(messageData.roomId);
+            console.log(messageData.roomId);
+            console.log(messageData.content)
             io.to(messageData.roomId).emit('message', messageData);
         })
         socket.on('check_room', (roomId) => {
@@ -77,3 +79,4 @@ module.exports = (server) => {
     });
     return io;
 }
+
